352b5785ed2cf3d3e503b428810e48a7ebb15aa4
TonyMorse92 <anthonymorse92@gmail.com>
2025-04-19 15:31:18 -0400

    Adding statsmodels to requirements file.

--- lin_reg.py
+++ lin_reg.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+
+x = np.arange(0,10,1)
+y = x + random.uniform(0,3)
+
+# y = mx + b
+slope = np.polyfit(x,y,1)
+poly1d_fn = np.poly1d(slope)
 
 
-x = np.arange(0,1,.1)
-y = x**2
+plt.plot(x, y, 'yo', x, poly1d_fn(x), '--k')
+
+plt.xlim(0,5)
+plt.ylim(0,12)
 
 
-plt.plot(x,y)
 plt.show()
